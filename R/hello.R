24b2ac7af330e69ef6e25df93d55ff33cf72eb5c
jugalindog <85702882+jugalindog@users.noreply.github.com>
2022-11-28 22:40:00 -0500

    updte

--- R/hello.R
+++ R/hello.R
@@ -1,26 +1,26 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
+#' Split a string
+#'
+#' @param x A character vector with one element.
+#' @param split What to split on.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' a= -4
+#' b= -4
+#' c=  4
+#' pancito(a,b,c)
+#'
 pancito <-  function(a,b,c) {
   j <- (-b+(sqrt((b**2)-(4*a*c)))/(2*a))
   k <- (-b-(sqrt((b**2)-(4*a*c)))/(2*a))
+  j = round(j,1)
+  k = round(k,1)
   ifelse(((b**2)-(4*a*c))>0,
-         print("El sistema tiene dos soluciones",c(j,k)),
+         paste(rep("Solucion", 2), seq(1,2,1), c(j,k), sep = " /", collapse = ", "),
+         #print(paste("El sistema tiene dos soluciones",collapse = c(j,k))),
           ifelse(((b**2)-(4*a*c)) == 0,
                  print(paste("El sistema tiene unica solucion",j)),
                  print("El sistema NO tiene solucones")))
 }
-
-pancito(-8,-4,4)
